5ee5dd804b85756ef44d44149b946d9c23440401
danielkwon89 <danielkwon89@gmail.com>
2020-06-08 15:24:11 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+headers = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4").map(&:text)
+# binding.pry
