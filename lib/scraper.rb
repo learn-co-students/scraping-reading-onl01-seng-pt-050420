63dd5006b34e238f73ea9d3874e08ab744bd2c85
github username <github email address>
2020-06-08 14:09:45 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,20 @@
+# require 'nokogiri'
+# require 'open-uri'
+
+# html = open("https://flatironschool.com/")
+
+# doc = Nokogiri::HTML(html)
+# doc.css(".headline-26OIBN").text
+
+
 require 'nokogiri'
 require 'open-uri'
+ 
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN").text.strip
+puts doc.css(".headline-26OIBN")
 
-html = open("https://flatironschool.com/")
+doc_two = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc_two.css(".buttonBlue-3yHkWz").text
+
+doc.css(".headline-26OIBN").text
